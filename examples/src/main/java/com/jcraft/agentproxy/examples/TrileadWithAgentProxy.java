fc803f51d1791e04d0148780b2022ca4997dc3ac
Charles Duffy <duffy@indeed.com>
2013-06-03 11:04:24 -0500

    Add example of Trilead SSH2 use with proxy authentication

---
+++ examples/src/main/java/com/jcraft/agentproxy/examples/TrileadWithAgentProxy.java
@@ -0,0 +1,58 @@
+package com.jcraft.jsch.agentproxy.examples;
+
+import com.jcraft.jsch.agentproxy.TrileadAgentFactory;
+import com.trilead.ssh2.Connection;
+import com.trilead.ssh2.Session;
+import com.trilead.ssh2.StreamGobbler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TrileadWithAgentProxy {
+    public static void main(String[] arg) throws IOException {
+        if(arg.length != 2) {
+            System.err.println("Usage: test-ssh user@host command");
+            System.exit(1);
+        }
+
+        int splitPoint = arg[0].indexOf("@");
+        if(splitPoint <= 0) {
+            System.err.println("Usage: test-ssh user@host command");
+            System.exit(1);
+        }
+
+        String user = arg[0].substring(0, splitPoint);
+        String host = arg[0].substring(splitPoint + 1);
+
+        com.trilead.ssh2.auth.AgentProxy agentProxy = TrileadAgentFactory.getAgentProxy();
+        if(agentProxy == null) {
+            System.err.println("ERROR: Unable to connect to SSH agent");
+            System.exit(1);
+        }
+
+        Connection conn = new Connection(host);
+        conn.connect();
+        boolean isAuthenticated = conn.authenticateWithAgent(user, agentProxy);
+        if(isAuthenticated == false) {
+            System.err.println("ERROR: Agent authentication not accepted");
+            System.exit(1);
+        }
+
+        Session sess = conn.openSession();
+        sess.execCommand(arg[1]);
+        BufferedReader br = new BufferedReader(new InputStreamReader(new StreamGobbler(sess.getStdout())));
+        while(true) {
+            String line = br.readLine();
+            if(line == null) {
+                break;
+            }
+            System.out.println(line);
+        }
+        Integer exitStatus = sess.getExitStatus();
+        sess.close();
+        conn.close();
+
+        System.exit(exitStatus == null ? 1 : exitStatus);
+    }
+}
