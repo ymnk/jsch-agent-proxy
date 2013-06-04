3e60e70f2c81c5ca6be2dbcaa73205ae5feae8f7
Charles Duffy <duffy@indeed.com>
2013-06-04 14:14:19 -0500

    Modify trilead-ssh2 patch per review feedback

--- examples/src/main/java/com/jcraft/agentproxy/examples/TrileadWithAgentProxy.java
+++ examples/src/main/java/com/jcraft/agentproxy/examples/TrileadWithAgentProxy.java
@@ -1,16 +1,23 @@
 package com.jcraft.jsch.agentproxy.examples;
 
-import com.jcraft.jsch.agentproxy.TrileadAgentFactory;
+import com.jcraft.jsch.agentproxy.AgentProxyException;
+import com.jcraft.jsch.agentproxy.Connector;
+import com.jcraft.jsch.agentproxy.TrileadAgentProxy;
+import com.jcraft.jsch.agentproxy.USocketFactory;
+import com.jcraft.jsch.agentproxy.connector.PageantConnector;
+import com.jcraft.jsch.agentproxy.connector.SSHAgentConnector;
+import com.jcraft.jsch.agentproxy.usocket.JNAUSocketFactory;
 import com.trilead.ssh2.Connection;
 import com.trilead.ssh2.Session;
 import com.trilead.ssh2.StreamGobbler;
+import com.trilead.ssh2.auth.AgentProxy;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class TrileadWithAgentProxy {
-    public static void main(String[] arg) throws IOException {
+    public static void main(String[] arg) throws IOException, AgentProxyException {
         if(arg.length != 2) {
             System.err.println("Usage: test-ssh user@host command");
             System.exit(1);
@@ -25,11 +32,18 @@ public class TrileadWithAgentProxy {
         String user = arg[0].substring(0, splitPoint);
         String host = arg[0].substring(splitPoint + 1);
 
-        com.trilead.ssh2.auth.AgentProxy agentProxy = TrileadAgentFactory.getAgentProxy();
-        if(agentProxy == null) {
+        Connector con;
+        if(SSHAgentConnector.isConnectorAvailable()) {
+            USocketFactory usf = new JNAUSocketFactory();
+            con = new SSHAgentConnector(usf);
+        } else if(PageantConnector.isConnectorAvailable()) {
+            con = new PageantConnector();
+        } else {
             System.err.println("ERROR: Unable to connect to SSH agent");
             System.exit(1);
+            return; /* compiler does not recognize System.exit as terminating */
         }
+        AgentProxy agentProxy = new TrileadAgentProxy(con);
 
         Connection conn = new Connection(host);
         conn.connect();
@@ -53,6 +67,6 @@ public class TrileadWithAgentProxy {
         sess.close();
         conn.close();
 
-        System.exit(exitStatus == null ? 1 : exitStatus);
+        System.exit(exitStatus == null ? 1 : exitStatus.intValue());
     }
 }
