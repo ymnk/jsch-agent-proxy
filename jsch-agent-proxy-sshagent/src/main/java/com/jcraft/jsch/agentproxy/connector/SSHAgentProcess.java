deaa3af8a00454de6298623af8c13a583693b34d
Mathieu Payeur Levallois <math.pay@gmail.com>
2014-09-26 22:16:09 +0100

    Better check around ssh-agent output parsing.

--- jsch-agent-proxy-sshagent/src/main/java/com/jcraft/jsch/agentproxy/connector/SSHAgentProcess.java
+++ jsch-agent-proxy-sshagent/src/main/java/com/jcraft/jsch/agentproxy/connector/SSHAgentProcess.java
@@ -3,6 +3,8 @@ package com.jcraft.jsch.agentproxy.connector;
 import java.io.IOException;
 import java.io.InputStream;
 
+import com.jcraft.jsch.agentproxy.AgentProxyException;
+
 public class SSHAgentProcess{
   private String socketPath;
   private Integer pid;
@@ -15,29 +17,33 @@ public class SSHAgentProcess{
     this.socketPath=socketPath;
   }
 
-  public SSHAgentProcess() throws IOException{
+  public SSHAgentProcess() throws AgentProxyException{
     this("/usr/bin/ssh-agent");
   }
 
-  public SSHAgentProcess(String sshAgentPath) throws IOException{
+  public SSHAgentProcess(String sshAgentPath) throws AgentProxyException{
     ProcessBuilder builder=new ProcessBuilder(sshAgentPath);
     builder.redirectErrorStream(true);
 
-    Process process=builder.start();
-    InputStream is=process.getInputStream();
+    try{
+      Process process=builder.start();
+      InputStream is=process.getInputStream();
 
-    byte[] buff=new byte[2048];
-    is.read(buff);
-    is.close();
+      byte[] buff=new byte[2048];
+      is.read(buff);
+      is.close();
 
-    String agentOutput=new String(buff);
-    String[] splitAgentOutput=agentOutput.split(";");
+      String agentOutput=new String(buff);
+      String[] splitAgentOutput=agentOutput.split(";");
 
-    String[] splitSocketPath=splitAgentOutput[0].split("=");
-    socketPath=splitSocketPath[1];
+      String[] splitSocketPath=splitAgentOutput[0].split("=");
+      socketPath=splitSocketPath[1];
 
-    String[] splitAgentPid=splitAgentOutput[2].split("=");
-    pid=Integer.valueOf(splitAgentPid[1]);
+      String[] splitAgentPid=splitAgentOutput[2].split("=");
+      pid=Integer.valueOf(splitAgentPid[1]);
+    }catch(Exception e){
+      throw new AgentProxyException("Unable to parse ssh-agent output.");
+    }
   }
 
   public void stopAgent() throws IOException {
